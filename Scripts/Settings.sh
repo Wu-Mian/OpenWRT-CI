4d98556fd7b645b473dcbf41f52407cea7578373
无眠 <32206048+Wu-Mian@users.noreply.github.com>
2024-10-19 23:51:06 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -15,6 +15,9 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='Asia/Shanghai'/g" $CFG_FILE
+
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
